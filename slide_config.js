584df9496a73b51945b952421a76fa49bfef33f5
Jakob Heuser <jakob@felocity.com>
2014-07-02 00:08:41 -0700

    updated to begin making more generic framework for this

--- slide_config.js
+++ slide_config.js
@@ -1,18 +1,18 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
-    //eventInfo: {
-    //  title: 'Google I/O',
-    //  date: '6/x/2013'
-    //},
+    title: 'A Pragmatic Approach<br>to Web Components',
+    subtitle: '',
+    eventInfo: {
+     title: 'Open Web Camp VI',
+     date: 'July 12, 2014'
+    },
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: '', // TODO
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -22,12 +22,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Jakob Heuser',
+    company: 'Principal Web Developer<br>LinkedIn',
+    // gplus: 'http://plus.google.com/1234567890',
+    twitter: '@jakobo',
+    www: 'http://felocity.com',
+    github: 'http://github.com/jakobo'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
@@ -37,4 +37,3 @@ var SLIDE_CONFIG = {
     github: 'http://github.com/you'
   }*/]
 };
-
